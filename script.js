de4c70130253752361f1824ab90d795fb0ffc0bb
Russel Amin Kadra P. Hanapi <92841378+russelhanapi@users.noreply.github.com>
2025-02-14 15:41:23 +0800

    feat: Render gameboard cells in the UI
    - Rendered an empty gameboard grid with 9 cells.
    - Each cell is represented by a `div` element with a unique ID.
    - The cells are currently empty and await player interaction.

---
+++ script.js
@@ -0,0 +1,29 @@
+// script.js
+
+const Gameboard = (function () {
+  const gameboardGrid = document.querySelector('#gameboard');
+  let board = Array(9).fill('');
+
+  // Render the gameboard in the UI
+  function render() {
+    let html = ``;
+    board.forEach(
+      (cell, index) =>
+        (html += `<div class="cell" id="cell-${index}">${cell}</div>`)
+    );
+    gameboardGrid.innerHTML = html;
+  }
+
+  // Start/Reset the gameboard
+  function reset() {
+    board.fill('', 0, 9);
+    render();
+  }
+
+  return {
+    render,
+    reset,
+  };
+})();
+
+Gameboard.render();
